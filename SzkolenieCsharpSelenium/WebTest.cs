fc16207c0409f4d0769ebd3fd3801e899cd72c44
pawelwch <pawelmaryniak@wp.pl>
2020-02-27 21:17:19 +0100

    new test added

--- SzkolenieCsharpSelenium/WebTest.cs
+++ SzkolenieCsharpSelenium/WebTest.cs
@@ -22,11 +22,22 @@ namespace SzkolenieCsharpSelenium
             driver.FindElement(By.Name("q")).SendKeys("Look4App");
             Thread.Sleep(1000);
             driver.FindElement(By.Name("btnK")).Click();
-
-            
             Assert.That(driver.Title.Contains("Look4App"));
-
             driver.Quit();
+        }
+
+        [Test]
+        public void CheckLook4AppChannelDescription()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.youtube.pl");
+            driver.FindElement(By.Id("search")).SendKeys("look4app");
+            driver.FindElement(By.Id("search-icon-legacy")).Click();
+            String channelDescription = driver.FindElement(By.Id("description")).Text;
+            StringAssert.Contains(channelDescription, "We do cutting-edge blockchain and web projects. We're in Top 5 Blockchain Companies according to Clutch.io.");
+            driver.Quit();
+
         }
     }
 }
